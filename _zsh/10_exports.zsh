478e614b82c0e8b1f0bd1cc59cfb6eb5c05c0f4d
superbrothers <superbrothers@shisobu.in>
2011-11-08 11:42:55 +0900

    vimのpathを動的に設定するように変更

--- _zsh/10_exports.zsh
+++ _zsh/10_exports.zsh
@@ -1,5 +1,5 @@
 export EDITOR=vim
-export GIT_EDITOR="/usr/local/bin/vim -c 'set fenc=utf-8'"
+export GIT_EDITOR="`which vim` -c 'set fenc=utf-8'"
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export __CF_USER_TEXT_ENCODING='0x1F5:0x08000100:14' # 文字コード設定
 export PATH=$HOME/bin:/usr/local/bin:$PATH
