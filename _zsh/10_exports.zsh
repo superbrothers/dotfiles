72a5dd4a6cefb474a95746a1c7320c7a1beb1508
superbrothers <superbrothers@shisobu.in>
2012-03-31 11:43:54 +0900

    PATHを変更

--- _zsh/10_exports.zsh
+++ _zsh/10_exports.zsh
@@ -2,5 +2,5 @@ export EDITOR=vim
 export GIT_EDITOR="`which vim` -c 'set fenc=utf-8'"
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export __CF_USER_TEXT_ENCODING='0x1F5:0x08000100:14' # 文字コード設定
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=/usr/local/bin:$PATH
 export MANPATH=$HOME/share/man:$MANPATH
