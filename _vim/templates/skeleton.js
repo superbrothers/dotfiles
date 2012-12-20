418b70d7b908a562636bcb9f892d4727edaccdc6
superbrothers <kazuki.suda@gmail.com>
2012-12-20 20:54:54 +0900

    Add nomen option

--- _vim/templates/skeleton.js
+++ _vim/templates/skeleton.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
-/*jslint devel: true, browser: true, node: true, sloppy: true, maxerr: 50, indent: 4 */
+/*jslint devel: true, browser: true, node: true, sloppy: true, nomen: true, maxerr: 50, indent: 4 */
 
 // vim: set fenc=utf-8 ts=4 sts=4 sw=4 :
