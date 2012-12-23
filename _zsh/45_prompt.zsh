1b4d26fbfe3e2a1749b215558aa0565a2403e322
superbrothers <kazuki.suda@gmail.com>
2012-12-23 20:31:27 +0900

    Use zsh-syntax-highlighting

--- _zsh/45_prompt.zsh
+++ _zsh/45_prompt.zsh
@@ -1,3 +1,3 @@
-PROMPT="%{$fg[magenta]%}${USER} %{$fg[white]%}in %{$fg[green]%}${HOST} %{$fg[white]%}at %{$fg[cyan]%}%~%{$reset_color%}
+PROMPT="%{$fg[red]%}${USER} %{$fg[white]%}in %{$fg[green]%}${HOST} %{$fg[white]%}at %{$fg[cyan]%}%~%{$reset_color%}
 %# "
 RPROMPT='$(rbenv_version)$(nodebrew_node_version)$(vcs_info_msg)[%D %T]'
