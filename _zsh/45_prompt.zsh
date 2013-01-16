ac0d3477b83f17c36b258273759dd0586d25dbe2
Kazuki Suda <ksuda@yahoo-corp.jp>
2013-01-16 13:52:40 +0900

    zshで右プロンプトを自動的に消す
    http://www.machu.jp/diary/20130114.html#p01

--- _zsh/45_prompt.zsh
+++ _zsh/45_prompt.zsh
@@ -1,3 +1,4 @@
 PROMPT="%B%(?.%F{green}(^_^%)%f.%F{red}(>_<%)%f)%b %{$fg[red]%}${USER} %{$fg[white]%}in %{$fg[green]%}${HOST} %{$fg[white]%}at %{$fg[cyan]%}%~%{$reset_color%}
 %# "
 RPROMPT='$(rbenv_version)$(nodebrew_node_version)$(vcs_info_msg)[%D %T]'
+setopt transient_rprompt
