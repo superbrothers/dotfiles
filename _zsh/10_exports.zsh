a229b522cc338b043960935f18cef20c4a4cc359
Kazuki Suda <kazuki.suda@gmail.com>
2013-04-30 15:59:36 +0900

    Update zsh files

--- _zsh/10_exports.zsh
+++ _zsh/10_exports.zsh
@@ -1,5 +1,6 @@
 export EDITOR=vim
 export GIT_EDITOR="`which vim` -c 'set fenc=utf-8'"
 export __CF_USER_TEXT_ENCODING='0x1F5:0x08000100:14' # 文字コード設定
-export PATH=/usr/local/bin:$PATH
+export PATH=$HOME/Dropbox/root/bin:$HOME/.nodebrew/current/bin:/usr/local/bin:$PATH
 export MANPATH=$HOME/share/man:$MANPATH
+export RBENV_ROOT=/usr/local/var/rbenv #Used Homebrew
