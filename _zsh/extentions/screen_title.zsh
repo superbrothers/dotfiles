3513ff93e07b48b8b513c260179c20e9e25b7c74
Kazuki Suda <kazuki.suda@gmail.com>
2013-05-22 19:11:09 +0900

    Show remote hostname during ssh in screen title

--- _zsh/extentions/screen_title.zsh
+++ _zsh/extentions/screen_title.zsh
@@ -1,10 +1,15 @@
+## Adding the following settings to .ssh/config
+## show remote hostname during ssh
+#
+# PermitLocalCommand yes
+# LocalCommand [[ "$TERM" =~ ^screen ]] && echo -ne "\ek$(echo %h | awk 'BEGIN{FS="."}{print $1}')\e\\"
+#
 autoload -Uz add-zsh-hook
 function _precmd_screen_title() {
-    echo -ne "\ek$(hostname|awk 'BEGIN{FS="."}{print $1}')\e\\"
+    echo -ne "\ek$(basename $SHELL)\e\\"
 }
 function _preexec_screen_title() {
-    mycmd=(${(s: :)${1}})
-    echo -ne "\ek$(hostname|awk 'BEGIN{FS="."}{print $1}'):$mycmd[1]\e\\"
+    echo -ne "\ek$1\e\\"
 }
 add-zsh-hook precmd _precmd_screen_title
 add-zsh-hook preexec _preexec_screen_title
