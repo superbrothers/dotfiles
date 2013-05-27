d1fb9d9563775985eeb0f03d0aca466b843db604
Kazuki Suda <kazuki.suda@gmail.com>
2013-05-27 18:41:08 +0900

    title にはコマンド名のみ表示するように変更

--- _zsh/extentions/screen_title.zsh
+++ _zsh/extentions/screen_title.zsh
@@ -4,12 +4,13 @@
 # PermitLocalCommand yes
 # LocalCommand [[ "$TERM" =~ ^screen ]] && echo -ne "\ek$(echo %h | awk -F "." '{print $1}')\e\\"
 #
+setopt extended_glob
 autoload -Uz add-zsh-hook
 function _precmd_screen_title() {
     echo -ne "\ek$(basename $SHELL)\e\\"
 }
 function _preexec_screen_title() {
-    echo -ne "\ek$1\e\\"
+    echo -ne "\ek${1/(#b)( *)/}\e\\"
 }
 add-zsh-hook precmd _precmd_screen_title
 add-zsh-hook preexec _preexec_screen_title
