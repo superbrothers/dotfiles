767f383ddb3ac7e85e65e95a0a3d01a9657cb123
Kazuki Suda <kazuki.suda@gmail.com>
2013-06-17 13:33:37 +0900

    display '{?}' if not installed ruby of .ruby-version

--- _zsh/extentions/rbenv.zsh
+++ _zsh/extentions/rbenv.zsh
@@ -5,11 +5,14 @@ autoload -U colors && colors
 
 rbenv_version() {
     if [ -n "$RBENV_ROOT" ]; then
+        VERSION=''
+        NOTFOUND=''
         if [ -r .ruby-version ]; then
             VERSION=`cat .ruby-version`
+            ls "${RBENV_ROOT}/versions" | egrep "^$VERSION$" >/dev/null 2>&1 || NOTFOUND='{?}'
         else
             VERSION=`cat "${RBENV_ROOT}/version"`
         fi
-        echo "%{$fg[red]%}[$VERSION]%{$reset_color%}"
+        echo "%{$fg[red]%}[$VERSION]$NOTFOUND%{$reset_color%}"
     fi
 }
