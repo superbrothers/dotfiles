af3b6e29a9b7e490981ab5d732b44a00d6b9bbd6
Kazuki Suda <kazuki.suda@gmail.com>
2013-06-17 14:11:50 +0900

    Update prompt format

--- _zsh/45_prompt.zsh
+++ _zsh/45_prompt.zsh
@@ -1,4 +1,4 @@
 PROMPT="%B%(?.%{$fg[green]%}(^_^%).%{$fg[red]%}(>_<%))%b %{$fg[red]%}${USER} %{$fg[white]%}in %{$fg[green]%}${HOST} %{$fg[white]%}at %{$fg[cyan]%}%~%{$reset_color%}
 %# "
-RPROMPT='$(rbenv_version)$(nodebrew_node_version)$(vcs_info_msg)[%D %T]'
+RPROMPT='$(vcs_info_msg)$(rbenv_version)$(nvm_node_version)[%D %T]'
 setopt transient_rprompt
