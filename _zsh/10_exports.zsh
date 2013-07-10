c1926dd0fd68babf57a93b95407af7bdbeb7b8d7
Kazuki Suda <kazuki.suda@gmail.com>
2013-07-10 16:58:12 +0900

    Add WORDCHARS

--- _zsh/10_exports.zsh
+++ _zsh/10_exports.zsh
@@ -4,3 +4,4 @@ export __CF_USER_TEXT_ENCODING='0x1F5:0x08000100:14' # 文字コード設定
 export PATH=$HOME/Dropbox/root/bin:$HOME/.nodebrew/current/bin:/usr/local/bin:$PATH
 export MANPATH=$HOME/share/man:$MANPATH
 export RBENV_ROOT=/usr/local/var/rbenv #Used Homebrew
+export WORDCHARS='*_[]~;!%^(){}<>'
