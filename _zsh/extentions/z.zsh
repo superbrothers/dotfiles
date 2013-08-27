00da5a2e2937007c15de59eac8d7e9a236587e35
Kazuki Suda <kazuki.suda@gmail.com>
2013-08-27 10:27:48 +0900

    Update

--- _zsh/extentions/z.zsh
+++ _zsh/extentions/z.zsh
@@ -1,9 +1,7 @@
 # brew install z
-if [[ -x "`which brew 2>/dev/null`" ]]; then
-    . `brew --prefix`/etc/profile.d/z.sh
-    autoload -Uz add-zsh-hook
-    function _Z_precmd () {
-        z --add "$(pwd -P)"
-    }
-    add-zsh-hook precmd _Z_precmd
-fi
+. `brew --prefix`/etc/profile.d/z.sh
+autoload -Uz add-zsh-hook
+function _Z_precmd () {
+    z --add "$(pwd -P)"
+}
+add-zsh-hook precmd _Z_precmd
