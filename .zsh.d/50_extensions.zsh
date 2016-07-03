ec9a83275f3b483b696994d6b26098c9e093e8e9
Kazuki Suda <kazuki.suda@gmail.com>
2016-07-03 10:37:02 +0900

    Use brew's zsh-syntax-highlighting instead of submodule

--- .zsh.d/50_extensions.zsh
+++ .zsh.d/50_extensions.zsh
@@ -1,3 +1,6 @@
 for script in $(find $(dirname $0)/extentions -type f -name "*.zsh" -maxdepth 2); do
     source $script
 done
+
+## brew info zsh-syntax-highlighting
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
