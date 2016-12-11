4d9ec48bb73185b56c56c153d236138fabeb8c6c
Kazuki Suda <kazuki.suda@gmail.com>
2016-12-11 22:29:10 +0900

    zsh: Make kubectl to respect env vars such as KUBECTL_CONTEXT

--- .zsh.d/extentions/kubectl.zsh
+++ .zsh.d/extentions/kubectl.zsh
@@ -3,6 +3,18 @@ if which kubectl >/dev/null 2>&1; then
     FOUND_KUBECTL=1
 fi
 
+## Make `kubectl` to respect environment variables such as KUBECTL_CONTEXT
+if [ "$FOUND_KUBECTL" -eq 1 ];then
+    function kubectl() {
+        command kubectl \
+            --context="$KUBECTL_CONTEXT" \
+            --namespace="$KUBECTL_NAMESPACE" \
+            --user="$KUBECTL_USER" \
+            --server="$KUBECTL_SERVER" \
+            "$@"
+    }
+fi
+
 ## Completion
 test "$FOUND_KUBECTL" -eq 1 && source <(kubectl completion zsh)
 
