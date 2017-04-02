938c814c35f93671c63f115fb187f507e5793188
Kazuki Suda <kazuki.suda@gmail.com>
2017-04-02 14:56:16 +0900

    zsh: github.com/superbrothers/zsh-kubectl-prompt

--- .zsh.d/45_prompt.zsh
+++ .zsh.d/45_prompt.zsh
@@ -1,4 +1,4 @@
 local ret_status="%(?:%{$fg_bold[green]%}# :%{$fg_bold[red]%}# )"
 PROMPT="${ret_status} %{$fg[cyan]%}%c%{$reset_color%} \$(vcs_info_msg) %{$reset_color%}"
-RPROMPT='%{$fg[blue]%}[$(kubernetes_prompt_info)]%{$reset_color%}'
+RPROMPT='%{$fg[blue]%}($ZSH_KUBECTL_PROMPT)%{$reset_color%}'
 setopt transient_rprompt
--- .zsh.d/extentions/kubectl.zsh
+++ .zsh.d/extentions/kubectl.zsh
@@ -19,38 +19,6 @@ fi
 test "$FOUND_KUBECTL" -eq 1 && source <(kubectl completion zsh)
 
 ## PROMPT
-setopt prompt_subst
-
-function kubernetes_current_context() {
-    local context="$(kubectl config current-context)"
-    [ $? -eq 0 ] && echo "$context" || echo "no context"
-}
-
-function kubernetes_current_cluster() {
-    local line="$(kubectl config get-contexts --no-headers | grep -e '^*')"
-    [ $? -eq 0 ] && echo "$line" | awk '{print $3}' || echo "no context"
-}
-
-function kubernetes_current_user() {
-    local line="$(kubectl config get-contexts --no-headers | grep -e '^*')"
-    [ $? -eq 0 ] && echo "$line" | awk '{print $4}' || echo "no context"
-}
-
-function kubernetes_current_namespace() {
-    local line="$(kubectl config get-contexts --no-headers | grep -e '^*')"
-    [ $? -eq 0 ] && echo "$line" | awk '{if($5!=NULL){print $5}else{print "default"}}' || echo "no context"
-}
-
-# cluster/namespace
-function kubernetes_prompt_info() {
-    local line="$(kubectl config get-contexts --no-headers | grep -e '^*')"
-    [ $? -eq 0 ] && echo "$line" | awk '{printf $3"/";if($5!=NULL){print $5}else{print "default"}}' || echo "no context"
-}
-
-if [ $FOUND_KUBECTL -eq 0 ]; then
-    function kubernetes_current_context() { echo "Could not find kubectl" }
-    function kubernetes_current_cluster() { echo "Could not find kubectl" }
-    function kubernetes_current_user() { echo "Could not find kubectl" }
-    function kubernetes_current_namespace() { echo "Could not find kubectl" }
-    function kubernetes_prompt_info() { echo "Could not find kubectl" }
-fi
+zsh_kubectl_prompt="$(ghq root)/github.com/superbrothers/zsh-kubectl-prompt/kubectl.zsh"
+[[ ! -f "$zsh_kubectl_prompt" ]] && ghq get superbrothers/zsh-kubectl-prompt
+source "$zsh_kubectl_prompt"
