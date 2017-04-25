54375fbe2bbffd88d4725f41a4cb6ce88ed70247
Kazuki Suda <kazuki.suda@gmail.com>
2017-04-25 22:34:36 +0900

    zsh: Fix to export WHALEBREW_INSTALL_PATH

--- .zsh.d/extentions/whalebrew.zsh
+++ .zsh.d/extentions/whalebrew.zsh
@@ -1,4 +1,4 @@
-WHALEBREW_INSTALL_PATH="$HOME/.whalebrew/bin"
+export WHALEBREW_INSTALL_PATH="$HOME/.whalebrew/bin"
 if [[ ! -d "$WHALEBREW_INSTALL_PATH" ]]; then
     mkdir -p "$WHALEBREW_INSTALL_PATH"
 fi
