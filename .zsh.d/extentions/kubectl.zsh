d1c4ef18eaecb2644228956fc1ab51a14c058d53
Kazuki Suda <kazuki.suda@gmail.com>
2018-04-12 10:09:41 +0900

    zsh: Alias kubectl to k

--- .zsh.d/extentions/kubectl.zsh
+++ .zsh.d/extentions/kubectl.zsh
@@ -1,6 +1,9 @@
 if which kubectl >/dev/null 2>&1; then
+  alias k='kubectl'
+
   ## Completion
   source <(kubectl completion zsh)
+  complete -o default -F __start_kubectl k
 
   ## PROMPT
   zsh_kubectl_prompt="$(brew --prefix)/etc/zsh-kubectl-prompt/kubectl.zsh"
