daf99e3554aeaf2048d2560e0f39bb9020671ae9
Kazuki Suda <kazuki.suda@gmail.com>
2020-08-06 11:48:57 +0900

    vim: Update a skeleton file for shell script

--- .vim/templates/skel.sh
+++ .vim/templates/skel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail
+set -e -o pipefail; [[ -n "$DEBUG" ]] && set -x
 
-[[ -n "$DEBUG" ]] && set -x
+SCRIPT_ROOT="$(cd "$(dirname "$0")"; pwd)"
 
 # vim: ai ts=2 sw=2 et sts=2 ft=sh
